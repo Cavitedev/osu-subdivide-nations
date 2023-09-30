aa118491cc82246527604e7d5bfb8c25017d8ae1
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-09-30 13:22:42 +0200

    Add options watch and browsers

--- extensions/restructuring/restructure/build.mjs
+++ extensions/restructuring/restructure/build.mjs
@@ -8,8 +8,8 @@ async function deleteOldDir() {
   await fs.remove(outdir);
 }
 
-async function runEsbuild(buildPath, manifestPath) {
-  return esbuild.context({
+async function runEsbuild({ buildPath, manifestPath, watch = false }) {
+  const esbuildOptions = {
     entryPoints: ["src/content-script/osu_content.ts"],
     bundle: true,
     outdir: buildPath,
@@ -27,22 +27,47 @@ async function runEsbuild(buildPath, manifestPath) {
         },
       }),
     ],
-  });
+  };
+
+  return watch
+    ? esbuild.context(esbuildOptions)
+    : esbuild.build(esbuildOptions);
 }
 
 async function build() {
+  const args = process.argv.slice(2);
+
+  let isChrome = args.includes("--chromium");
+  let isFirefox = args.includes("--firefox");
+  if (!isChrome && !isFirefox) {
+    isChrome = true;
+    isFirefox = true;
+  }
+
+  const isWatch = args.includes("--watch");
   await deleteOldDir();
-  const chromeBuild = await runEsbuild(
-    `./${outdir}/chromium`,
-    "src/manifest_chromium.json",
-  );
-  const firefoxBuild = await runEsbuild(
-    `./${outdir}/firefox`,
-    "src/manifest_firefox.json"
-  );
+
+  let chromeBuild, firefoxBuild;
+  if (isChrome) {
+    chromeBuild = await runEsbuild({
+      buildPath: `./${outdir}/chromium`,
+      manifestPath: "src/manifest_chromium.json",
+      watch: isWatch,
+    });
+  } else if (isFirefox) {
+    firefoxBuild = await runEsbuild({
+      buildPath: `./${outdir}/firefox`,
+      manifestPath: "src/manifest_firefox.json",
+      watch: isWatch,
+    });
+  }
+
   console.log("Build success");
-  chromeBuild.watch();
-  await firefoxBuild.watch();
+
+  if (isWatch) {
+    await Promise.all([chromeBuild?.watch(), firefoxBuild?.watch()]);
+    console.log("Watching for changes...");
+  }
 }
 
 build();
