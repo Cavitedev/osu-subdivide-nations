503bf96c41ece967747a0230e1fe2230f8e0393a
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-09-30 11:59:32 +0200

    Copy manifests

--- extensions/restructuring/restructure/build.mjs
+++ extensions/restructuring/restructure/build.mjs
@@ -1,24 +1,59 @@
 import esbuild from "esbuild";
 import fs from "fs-extra";
-
+import { copy } from "esbuild-plugin-copy";
 
 const outdir = "build";
+const tmp_dir = "tmp_build";
 
 async function deleteOldDir() {
   await fs.remove(outdir);
 }
 
-async function runEsbuild() {
+async function runEsbuild(buildPath, manifestPath) {
   await esbuild.build({
     entryPoints: ["src/content-script/osu_content.ts"],
     bundle: true,
-    outdir: outdir,
+    outdir: buildPath,
+
+    assetNames: "[assets/[name]-[hash]]",
+    plugins: [
+      copy({
+        assets: {
+          from: ["./src/assets/**"],
+          to: ["./assets"],
+        },
+      }),
+      copy({
+        assets: {
+          from: [manifestPath],
+          to: ["./manifest.json"],
+        },
+      }),
+    ],
   });
+}
+
+async function copyFiles(entryPoints, targetDir) {
+  await fs.ensureDir(targetDir);
+  await Promise.all(
+    entryPoints.map(async (entryPoint) => {
+      await fs.copy(entryPoint.src, `${targetDir}/${entryPoint.dst}`);
+    })
+  );
 }
 
 async function build() {
   await deleteOldDir();
-  await runEsbuild();
+  await runEsbuild(`./${outdir}/chromium`, "src/manifest_chromium.json");
+  await runEsbuild(`./${outdir}/firefox`, "src/manifest_firefox.json");
+
+  // await fs.copy(tmp_dir);
+  // await fs.copy(tmp_dir, `./${outdir}/firefox`);
+
+  // await copyFiles([{ src: "", dst: "manifest.json" }], `./${outdir}/chromium`);
+
+  // await fs.remove(tmp_dir);
+
   console.log("Build success");
 }
 
---
+++ extensions/restructuring/restructure/build/firefox/osu_content.js
@@ -0,0 +1,5 @@
+"use strict";
+(() => {
+  // src/content-script/osu_content.ts
+  console.log("osu_content.ts");
+})();
