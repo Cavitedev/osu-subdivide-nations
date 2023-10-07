b23b98de9f90cad42ca662db9eb5991c370ad2d9
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-10-07 15:54:26 +0200

    Fix load from cache Firefox

--- extension/src/utils/cache.ts
+++ extension/src/utils/cache.ts
@@ -4,7 +4,7 @@ export const saveInCache = async (url:string, data:object) => {
   
   export const loadFromCache =async  (url:string): Promise<any | null> => {
     const storageReturn = await chrome.storage.local.get([url]);
-    if(Object.keys(storageReturn).length === 0){
+    if(!storageReturn ||  Object.keys(storageReturn).length === 0){
       return null;
     }else{
       return storageReturn[url];
