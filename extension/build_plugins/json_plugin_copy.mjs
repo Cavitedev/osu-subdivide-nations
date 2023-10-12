3b8fe66c3ebb7fc71b06e58adcd31a47176fb7c0
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-10-12 13:23:43 +0200

    Mignify only in build mode

--- extension/build_plugins/json_plugin_copy.mjs
+++ extension/build_plugins/json_plugin_copy.mjs
@@ -8,18 +8,23 @@ export default ({ resolveFrom }) => ({
   setup(build) {
     build.onStart(async () => {
       const outdir = build.initialOptions.outdir;
+      const mignify = build.initialOptions.minify;
 
       const cwd = process.cwd();
-
-      const relativePath = path.relative(cwd, resolveFrom);
 
       const files = await glob(resolveFrom + ".json");
 
       for (const file of files){
         const jsonFilePath = path.resolve(cwd, file);
         const jsonContents = await fs.readFile(jsonFilePath, "utf8");
-        let minifiedJson = jsonminify(jsonContents);
-        if(!minifiedJson) minifiedJson = "{}";
+        let minifiedJson
+        if(mignify){
+          minifiedJson = jsonminify(jsonContents);
+          if(!minifiedJson) minifiedJson = "{}";
+        }else{
+          minifiedJson = jsonContents;
+        }
+
 
         const outFilePath = path.resolve(outdir, file).replace("\\src", "");
         fs.outputFileSync(outFilePath, minifiedJson, {
