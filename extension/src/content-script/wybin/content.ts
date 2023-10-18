326a0d9d9452eb7d57bd48533442f4e93a703191
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-10-19 00:24:28 +0200

    Remove logs

--- extension/src/content-script/wybin/content.ts
+++ extension/src/content-script/wybin/content.ts
@@ -5,12 +5,10 @@ import { updateFlagsTeams } from "./pages/teams";
 
 
 const contentObserver = new MutationObserver(() => {
-    console.log("content");
     exec();
 });
 
 const titleObserver = new MutationObserver(() => {
-    console.log("title");
     exec();
 });
 
@@ -20,7 +18,6 @@ let content: HTMLElement | undefined = undefined;
 
 export const exec = async () => {
 
-    console.log("exec");
     titleObserver.observe(document.querySelector("head > title")!, {childList: true});
     
     content = document.querySelector("body > app-root > app-tournament-view > div.content-spacing > app-tournament-view-details") as HTMLElement;
@@ -30,7 +27,9 @@ export const exec = async () => {
     contentObserver.observe(contentChild ?? content, {childList: true});
     
     //No content
-    if(content.children.length === 0) return;
+    if(content.children.length === 0) {
+        return;
+    };
 
 
     updateFlagsPlayers();
