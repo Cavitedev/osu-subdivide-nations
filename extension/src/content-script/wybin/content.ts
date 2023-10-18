d642145dc1dc0f631e9dd182d8bc0f1ca28718a6
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-10-19 01:02:25 +0200

    Remove duplicated calls exec

--- extension/src/content-script/wybin/content.ts
+++ extension/src/content-script/wybin/content.ts
@@ -5,32 +5,38 @@ import { updateFlagsTeams } from "./pages/teams";
 
 
 const contentObserver = new MutationObserver(() => {
+    console.log("content");
+    exec();
+    contentObserver.disconnect();
+});
+
+const titleObserver = new MutationObserver((mut) => {
+    console.log(mut);
     exec();
 });
 
-const titleObserver = new MutationObserver(() => {
-    exec();
-});
+
 
 export const getContent = () => content;
 
 let content: HTMLElement | undefined = undefined;
 
 export const exec = async () => {
-    console.log("exec");
     titleObserver.observe(document.querySelector("head > title")!, {childList: true});
     
     content = document.querySelector("body > app-root > app-tournament-view > div.content-spacing > app-tournament-view-details") as HTMLElement;
     if(!content) return;
-    const contentChild = content.children[0] as HTMLElement;
-
-    contentObserver.observe(contentChild ?? content, {childList: true});
+ 
     
     //No content
     if(content.children.length === 0) {
+        const contentChild = content.children[0] as HTMLElement;
+        contentObserver.observe(contentChild ?? content, {childList: true});
         return;
     };
 
+    const starInserted = content.querySelector(".ng-star-inserted");
+    contentObserver.observe(starInserted!, {childList: true});
 
     updateFlagsPlayers();
     updateFlagsTeams();
