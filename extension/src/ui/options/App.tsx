49dfa0cb2a121fadca4c4e18c3e639b96c4d0285
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-02 23:24:48 +0100

    Working on linter

--- extension/src/ui/options/App.tsx
+++ extension/src/ui/options/App.tsx
@@ -1,25 +1,21 @@
-import { createSignal } from 'solid-js'
-import './App.css'
+import { createSignal } from "solid-js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+    const [count, setCount] = createSignal(0);
 
-  return (
-    <>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Vite + Solid + XD</h1>
+            <div class="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count()}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
+        </>
+    );
 }
 
-export default App
+export default App;
--- extension/src/ui/options/index.tsx
+++ extension/src/ui/options/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
+import { Router } from "@solidjs/router";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+    () => (
+        <Router>
+            <App />
+        </Router>
+    ),
+    root!,
+);
