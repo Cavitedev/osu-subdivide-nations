b6a551bf5a8a2dca79806771deaf8f24e23539c4
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-02 23:35:26 +0100

    Remove router

--- extension/src/ui/options/index.tsx
+++ extension/src/ui/options/index.tsx
@@ -3,15 +3,12 @@ import { render } from "solid-js/web";
 
 import "./index.css";
 import App from "./App";
-import { Router } from "@solidjs/router";
 
 const root = document.getElementById("root");
 
 render(
     () => (
-        <Router>
             <App />
-        </Router>
     ),
     root!,
 );
