83b0287ea437d5804571a5f5b24d61538df936c3
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-03 16:41:06 +0100

    Finish setting up tailwind

--- extension/src/ui/options/content/pages/Settings.tsx
+++ extension/src/ui/options/content/pages/Settings.tsx
@@ -1,15 +1,24 @@
-import "flowbite";
-
 export default function Settings() {
     return (
-        <div>
-            <h1>Header</h1>
-            <div>
-                <label class="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" class="sr-only peer" />
-                    <div class="w-11 h-6 bg-gray-400 peer-focus:outline-none  rounded-full  dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    <span class="ml-3 text-sm font-medium dark:text-gray-300">Toggle me</span>
-                </label>
+        <div class="mx-2">
+            <h2 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Global Settings</h2>
+            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 ">
+                <div class="flex">
+                    <div>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Score Ranking
+                        </h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">
+                            Displays Score Ranking from Repektive's API in osu! profile.
+                        </p>
+                    </div>
+                    <div>
+                        <label class="relative inline-flex items-center cursor-pointer">
+                            <input type="checkbox" value="" class="sr-only peer" />
+                            <div class="w-11 h-6 bg-gray-400 peer-focus:outline-none  rounded-full  dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
+                        </label>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
         "./node_modules/flowbite/**/*.js", // configure the Flowbite JS source template paths
