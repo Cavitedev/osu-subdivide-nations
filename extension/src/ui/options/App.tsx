c5cb1e7c3e967b1bf879b2b91192089e2278b743
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-04 13:19:31 +0100

    Change sync storage when changing toggle

--- extension/src/ui/options/App.tsx
+++ extension/src/ui/options/App.tsx
@@ -5,10 +5,11 @@ import "flowbite";
 
 function App() {
     return (
-        <div class="p-2 m-2 flex h-full transition-all duration-[0.4s] ease-[ease]">
-            <Sidebar />
-            <Content />
-            {/* <div>
+        
+            <div class="p-2 m-2 flex h-full transition-all duration-[0.4s] ease-[ease]">
+                <Sidebar />
+                <Content />
+                {/* <div>
                 <h1>Vite + Solid + XD</h1>
                 <div class="card">
                     <button onClick={() => setCount((count) => count + 1)}>count is {count()}</button>
@@ -18,7 +19,7 @@ function App() {
                 </div>
                 <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
             </div> */}
-        </div>
+            </div>
     );
 }
 
