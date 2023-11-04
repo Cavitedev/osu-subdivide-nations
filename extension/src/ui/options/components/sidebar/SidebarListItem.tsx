ed9f1266fc2a9729f22d64c2cb2b9344a901d35a
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-04 18:30:48 +0100

    Refactor and fix navigation sidebar

--- extension/src/ui/options/components/sidebar/SidebarListItem.tsx
+++ extension/src/ui/options/components/sidebar/SidebarListItem.tsx
@@ -1,19 +1,27 @@
 import { JSX } from "solid-js/jsx-runtime";
+import { useHashContext } from "../../context/hashContext";
 
 export default function SidebarListItem(props: {
     hash: string;
     icon: number | boolean | Node | JSX.ArrayElement | (string & {}) | null | undefined;
     label: number | boolean | Node | JSX.ArrayElement | (string & {}) | null | undefined;
 }) {
+    const { hash, setHash } = useHashContext();
+
+    const active = () => {
+        return hash() === `#${props.hash}` || (hash() === "" && props.hash === "settings");
+    };
+
     return (
         <li>
             <a
                 href={`#${props.hash}`}
                 onClick={(e) => {
                     e.preventDefault();
-                    window.location.hash = props.hash;
+                    setHash(props.hash);
                 }}
                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                classList={{ "bg-gray-200 dark:bg-gray-600": active() }}
             >
                 <svg
                     class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
