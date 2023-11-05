277dbca49ee547147baf68919708ff65beea60d5
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-11-05 16:59:12 +0100

    Improve html margins

--- extension/src/ui/options/content/pages/SettingsLayout.tsx
+++ extension/src/ui/options/content/pages/SettingsLayout.tsx
@@ -9,10 +9,10 @@ export default function SettingsLayout(props: {
     const title = locMsgToHtml(getLocMsg("website_settings"), [
         { type: "A", link: props.link, match: "Website", forcedString: props.websiteName },
     ]);
-    
+
     return (
         <div class="mx-2">
-            <h2 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h2>
+            <h2 class="mb-2 ml-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h2>
             {props.children}
         </div>
     );
